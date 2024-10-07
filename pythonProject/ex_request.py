c7a290b1201433a9b97c9775c34ac040a428c173
1BIN <wnbn231@gmail.com>
2024-10-07 15:40:09 +0900

    ~

---
+++ pythonProject/ex_request.py
@@ -0,0 +1,13 @@
+import requests
+url = "http://127.0.0.1:8080/api/addCode"
+# post 보낼 데이터
+data = {
+    "commCd" : "AB01"
+    ,"commNm" : "Test"
+    ,"commOrd": 1
+}
+# 요청 헤더
+headers = {"Content-Type": "application/json"}
+res = requests.post(url, json=data, headers=headers)
+print(f"status code:{res.status_code}")
+print(f"body: {res.text}")

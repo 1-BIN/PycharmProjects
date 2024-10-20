6a0e067f0b7dd38d34550c0a59c8c225fc41e9a2
1-BIN <wnbn231@gmail.com>
2024-10-20 23:56:50 +0900

    19일꺼

--- myProject/pythonProject/coin_project/get_news.py
+++ myProject/pythonProject/coin_project/get_news.py
@@ -40,10 +40,10 @@ while True:
 # 2. 마지막 페이지에 있는 오늘의 첫 기사까지 수집합니다.
 # (1) 수집할 내용은 고유번호, 날짜, 시간, 제목, 요약내용, 상세링크, 언론사
 article = soup.select('.kXHocr a')
-count = 1
+now = str(datetime.now())
+today = date.today()
 for dtl in reversed(article):
-    now = str(datetime.now())
-    onum = int(re.sub("[^0-9]+", '', now)[:8] + str(count).zfill(4))
+    # onum = int(re.sub("[^0-9]+", '', now)[:8] + str(count).zfill(4))
     article_dt = datetime.strptime(dtl.select_one('.glLXQX').text[5:18], "%Y년 %m월 %d일").date()  #날짜
     article_tm = dtl.select_one('.glLXQX span').text        #시간
     article_tt = dtl.select_one('.JpxIZ h3').text           #제목
@@ -51,36 +51,46 @@ for dtl in reversed(article):
     article_url = dtl.get('href')                           #링크
     article_jn = str(dtl.get('href')).split(".")[1]         #언론사(eng)
 
-    old_news_sql = f"""
+    old_news_url_sql = f"""
         SELECT article_url
         FROM news
-        WHERE article_url = '{article_url}'
     """
-    cur.execute(old_news_sql)
-    old_new_url_list = cur.fetchall()
-    print(old_new_url_list)
-    # if old_new_url_list
-
-
-
-
-
-    # insert_data = """
-    #     INSERT INTO news(onum
-    #                     , article_dt
-    #                     , article_tm
-    #                     , article_tt
-    #                     , article_con
-    #                     , article_url
-    #                     , article_jn )
-    #     VALUES(:1, :2, :3, :4, :5, :6, :7)
-    # """
-    # cur.execute(insert_data, ( onum
-    #                            ,article_dt
-    #                            ,article_tm
-    #                            ,article_tt
-    #                            ,article_con
-    #                            ,article_url
-    #                            ,article_jn))
-#     count = count+1
-# conn.commit()
+    cur.execute(old_news_url_sql)
+    old_news_url_list = cur.fetchall()
+    # 수집한 url이 데이터베이스에 없는 경우,
+    for old_url in old_news_url_list:
+        if old_url != article_url:
+            latest_article_sql = """
+                    SELECT onum, article_url, article_dt
+                    FROM (
+                        SELECT rownum, onum, article_url, article_dt
+                        FROM news
+                        ORDER BY 2 DESC
+                        )
+                    WHERE rownum = 1
+                """
+            cur.execute(latest_article_sql)
+            latest_article = cur.fetchall()
+            if list(latest_article[0])[2] == article_dt:
+                onum = list(latest_article[0])[0] + 1
+            elif today != list(latest_article[0])[2]:
+                onum = int(re.sub("[^0-9]+", '', str(article_dt)) + str(1).zfill(4))
+            # 새로운 기사 수집
+            insert_data = """
+                INSERT INTO news(onum
+                                , article_dt
+                                , article_tm
+                                , article_tt
+                                , article_con
+                                , article_url
+                                , article_jn )
+                VALUES(:1, :2, :3, :4, :5, :6, :7)
+            """
+            cur.execute(insert_data, ( onum
+                                       ,article_dt
+                                       ,article_tm
+                                       ,article_tt
+                                       ,article_con
+                                       ,article_url
+                                       ,article_jn))
+        conn.commit()
--- myProject/pythonProject/coin_project/test_code.py
+++ myProject/pythonProject/coin_project/test_code.py
@@ -1,4 +1,12 @@
 
 from datetime import date, timedelta, datetime
-today = date.today()
+
+today = datetime.strptime(str(date.today()), "%Y-%m-%d")
+today1 = date.today()
+formatted = today.strftime('%Y년 %m월 %d일')
+now = str(datetime.now())
 print(today)
+print(today1)
+print(formatted)
+print(now)
+
